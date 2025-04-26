0a80132889f259fd70b93d5b7b50991ad5376ff7
Daisy Choi <chj15937@gmail.com>
2025-04-26 23:33:22 +1200

    add sqlite database

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,10 @@
+using WebApplication1.Data;
 using WebApplication1.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
